53b5616381638bb4d41b97a2662c5f2ee3e05eac
Yujia Gu <yujiagu1010@gmail.com>
2022-05-24 15:05:15 +0930

    refactor task manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,49 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:details, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
+  end
+
+  def details
+    @message =
+      if @task.completed
+        'Well done! This task is completed.'
+      else
+        'This task is not completed.'
+      end
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def add
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new', as: :new
+  post 'tasks', to: 'tasks#add', as: :add
+  get 'tasks/:id', to: 'tasks#details', as: :task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update', as: :update
+  delete 'tasks/:id', to: 'tasks#destroy', as: :destroy
 end
